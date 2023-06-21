e8a0e690ce2fe914f94ecbdd592b6f8fe7af3d12
elenyel <acuhangel@gmail.com>
2023-06-21 01:04:37 -0600

    revisar los 3 elementos

--- index.php
+++ index.php
@@ -1,11 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inicio</title>
+  <link rel="stylesheet" href="src\css\normalize.css">
+  <link rel="stylesheet" href="src\css\style.css">
+
 </head>
+
 <body>
-    
+  <div class="container flex f-col f-center-primary center">
+
+    <!-- header -->
+
+    <!-- video -->
+    <section class="flex f-col f-center-primary">
+      <p class="font-size-50 intro-book video-txt center">
+        Empresa Tlaxcalteca
+        comprometida con nuestros
+        clientes
+      </p>
+
+      <iframe width="1024" height="720" src="https://www.youtube.com/embed/7DBIR30ks64" title="YouTube video player"
+        class="center" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+
+    </section>
+
+    <br>
+    <section>
+
+    </section>
+    <!-- parrafo -->
+    <section>
+      <p class="intro-light font-size-38 descripcion-txt center">Somos una empresa dedicada a las artes gráficas,
+        especializada en
+        servicios de
+        impresión y empaque con experiencia de más de 15 años en la industria.
+
+        <br><br>
+        Teniendo una infraestructura en planta de 3,551m2 hacemos presencia en
+        más de 100 paises en 4 continentes y nos enorgullecemos de ofrecer soluciones de
+        empaque de alta calidad y a medida para nuestros clientes.
+        <br><br>
+        Gracias al esfuerzo y la eficiencia de nuestro personal, hemos logrado alcanzar una
+        producción mensual de 20 millones de productos de empaque, lo que nos sitúa como
+        líderes en el mercado.
+      </p>
+
+    </section>
+    <!-- mapa   -->
+    <section>
+      <img class="img-100" src="src\images\Recurso 1.png" alt="">
+    </section>
+    <!-- seccion de 3 fotos -->
+    <section>
+        <div class="background_red flex f-wrap wrap_desk f-center-primary ">
+          <div class="division_section"></div>
+          <div class="flex f-col f-center-primary text-align-center width_30">
+            <img class="round_img" src="src/images/magnifier.jpg" alt="" />
+            <p class="font_size_40">Servicios</p>
+            <p class="">
+              Impresión offset hasta 6 colores en linea más barniz
+            </p>
+          </div>
+
+          <div class="flex f-col f-center-primary text-align-center width_30">
+            <img class="round_img" src="src/images/speedmaster.jpg" alt="" />
+            <p class="font_size_40">Servicios</p>
+            <p class="intro_light_reg width_30">
+              Equipo de vanguardia y técnologia de punta.
+            </p>
+          </div>
+
+          <div class="flex F-col f-center-primary text-align-center width_30">
+            <img class="round_img" src="src/images/group.jpeg" alt="" />
+            <p class="font_size_40">Responsabilidad social</p>
+            <p class="intro_light_reg width_30">
+              Valor humano, trato personal e integridad.
+            </p>
+          </div>
+        </div>
+  </div>
 </body>
+
 </html>
